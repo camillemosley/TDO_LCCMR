a9845f9c66bc13fe1c38b303e7f78b425a699fee
Michael McGuigan <mcgui622@umn.edu>
2025-09-24 17:58:05 -0500

    whatever message you want to include in your push

--- sci_museum_logger_data_formatting.r
+++ sci_museum_logger_data_formatting.r
@@ -28,36 +28,37 @@ BT4_buoy_formatted <- BT4_buoy %>%
                         DOperc_6.5,
                         Temp_7.5,
                         DO_adj_mod_bot,
-                        DO_perc_mod_bot),
+                        DO_perc_mod_bot,
+                        AbsPres_kPa),
                names_to = 'Variable',
                values_to = 'Value') %>%
-  add_column(Rep = NA)
-BT4_buoy_formatted$Depth <- word(BT4_buoy_formatted$Variable, -1, sep = '_')
-BT4_buoy_formatted$Rep[BT4_buoy_formatted$Variable == 'Temp_bot_2'] <- 2
-BT4_buoy_formatted$Rep[BT4_buoy_formatted$Variable != 'Temp_bot_2'] <- 1
-BT4_buoy_formatted$Depth[BT4_buoy_formatted$Variable == 'Temp_bot_2'] <- 'bot'
-BT4_buoy_formatted$Depth[BT4_buoy_formatted$Depth == 'bot'] <- 0
-BT4_buoy_formatted$Variable <- word(BT4_buoy_formatted$Variable, 1, -2, sep = '_')
-BT4_buoy_formatted$Variable[BT4_buoy_formatted$Variable == 'Temp_bot'] <- 'Temp'
-BT4_buoy_formatted$Depth <- as.numeric(BT4_buoy_formatted$Depth)  
+  mutate(Rep = case_when(grepl('bot_2', Variable) ~ 2,
+                         .default = 1),
+         Date_Time = as.POSIXct(paste(Date, Time), format = "%m/%d/%Y %H:%M:%S"),
+         Depth_m = as.numeric(word(Variable, -1, sep = '_'))) %>%
+  select(-Date, -Time, -X.) %>%
+  mutate(Depth_m = case_when(grepl('bot', Variable) ~ 0,
+                           grepl('kPa', Variable) ~ 0,
+                           .default = Depth_m),
+         Variable = case_when(grepl('Temp', Variable) ~ 'Temp',
+                              grepl('bot', Variable) ~ str_remove(Variable, '_bot'),
+                              grepl('_6.5', Variable) ~ str_remove(Variable, '_6.5'),
+                              .default = Variable))
 
-plot(BT4_buoy_formatted$Value[BT4_buoy_formatted$Variable == 'Temp' & BT4_buoy_formatted$Depth == 1.5], type = 'l', col = 'purple', ylim = c(0,26),
-     ylab = "Temperature and DO", xlab = "Time")
-lines(BT4_buoy_formatted$Value[BT4_buoy_formatted$Variable == 'Temp' & BT4_buoy_formatted$Depth == 2.5], col = 'blue')
-lines(BT4_buoy_formatted$Value[BT4_buoy_formatted$Variable == 'Temp' & BT4_buoy_formatted$Depth == 3.5], col = 'green')
-lines(BT4_buoy_formatted$Value[BT4_buoy_formatted$Variable == 'Temp' & BT4_buoy_formatted$Depth == 4.5], col = 'yellow')
-lines(BT4_buoy_formatted$Value[BT4_buoy_formatted$Variable == 'Temp' & BT4_buoy_formatted$Depth == 5.5], col = 'orange')
-lines(BT4_buoy_formatted$Value[BT4_buoy_formatted$Variable == 'Temp' & BT4_buoy_formatted$Depth == 7.5], col = 'red')
-lines(BT4_buoy_formatted$Value[BT4_buoy_formatted$Variable == 'Temp' & BT4_buoy_formatted$Depth == 0], col = 'black')
-lines(BT4_buoy_formatted$Value[BT4_buoy_formatted$Variable == 'DO' & BT4_buoy_formatted$Depth == 6.5], col = 'orange')
-lines(BT4_buoy_formatted$Value[BT4_buoy_formatted$Variable == 'DO' & BT4_buoy_formatted$Depth == 0], col = 'black')
+ggplot(BT4_buoy_formatted, aes(x = Date_Time, y = Value, linetype = Variable)) +
+  geom_line(data = subset(BT4_buoy_formatted, Variable == 'Temp' & Rep == 1), aes(colour = as.factor(Depth_m))) +
+  geom_line(data = subset(BT4_buoy_formatted, Variable == 'DO'), aes(colour = as.factor(Depth_m))) +
+  scale_linetype_manual(values = c('longdash', 'solid')) +
+  labs(colour = 'Depth (m)', y = 'mg/L | ºC')
 
 # this one measures depth from the bottom
 
 BT4_buoy_formatted <- BT4_buoy_formatted %>%
-  rename(Height_m = 'Depth')
-BT4_buoy_formatted$Depth_Adj_m <- abs(BT4_buoy_formatted$Height_m - BT4_buoy_formatted$Bot_depth)
+  rename(Height_m = 'Depth_m') %>%
+  mutate(Depth_Adj_m = abs(Height_m - Bot_depth)) %>%
+  mutate(Depth_m = abs(Height_m - unique(Height_m[Date_Time == max(Date_Time)])))
 BT4_buoy_formatted$Depth_m <- abs(BT4_buoy_formatted$Height_m - 9.905)
+BT4_buoy_formatted$Depth_m <- abs(BT4_buoy_formatted$Height_m[BT4_buoy_formatted$Depth_m[BT4_buoy_formatted$Date_Time == max(BT4_buoy_formatted$Date_Time)]])
 
 BT4_buoy_formatted <- BT4_buoy_formatted %>%
   rename(Bottom_Depth_m = 'Bot_depth',
@@ -96,6 +97,20 @@ BT5_buoy2016_formatted$Depth[BT5_buoy2016_formatted$Depth == 'bot'] <- 0
 BT5_buoy2016_formatted$Variable <- word(BT5_buoy2016_formatted$Variable, 1, -2, sep = '_')
 BT5_buoy2016_formatted$Variable[BT5_buoy2016_formatted$Variable == 'Temp_bot'] <- 'Temp'
 BT5_buoy2016_formatted$Depth <- as.numeric(BT5_buoy2016_formatted$Depth)
+
+# plot data from each logger
+plot(BT5_buoy2016_formatted$Value[BT5_buoy2016_formatted$Variable == 'Temp' & BT5_buoy2016_formatted$Depth == 1.5], type = 'l', col = 'purple', ylim = c(0,26),
+     ylab = "Temperature and DO", xlab = "Time")
+lines(BT5_buoy2016_formatted$Value[BT5_buoy2016_formatted$Variable == 'Temp' & BT5_buoy2016_formatted$Depth == 2.5], col = 'blue')
+lines(BT5_buoy2016_formatted$Value[BT5_buoy2016_formatted$Variable == 'Temp' & BT5_buoy2016_formatted$Depth == 3.5], col = 'green')
+lines(BT5_buoy2016_formatted$Value[BT5_buoy2016_formatted$Variable == 'Temp' & BT5_buoy2016_formatted$Depth == 4.5], col = 'yellow')
+lines(BT5_buoy2016_formatted$Value[BT5_buoy2016_formatted$Variable == 'Temp' & BT5_buoy2016_formatted$Depth == 5.5], col = 'orange')
+lines(BT5_buoy2016_formatted$Value[BT5_buoy2016_formatted$Variable == 'Temp' & BT5_buoy2016_formatted$Depth == 7.5], col = 'red')
+lines(BT5_buoy2016_formatted$Value[BT5_buoy2016_formatted$Variable == 'Temp' & BT5_buoy2016_formatted$Depth == 0], col = 'black')
+lines(BT5_buoy2016_formatted$Value[BT5_buoy2016_formatted$Variable == 'DO' & BT5_buoy2016_formatted$Depth == 6.5], col = 'orange')
+lines(BT5_buoy2016_formatted$Value[BT5_buoy2016_formatted$Variable == 'DO' & BT5_buoy2016_formatted$Depth == 0], col = 'black')
+
+# this one also measures depth from the bottom
 
 BT5_buoy2016_formatted <- BT5_buoy2016_formatted %>%
   rename(Height_m = 'Depth')
